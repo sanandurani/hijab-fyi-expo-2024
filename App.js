fc1f09a06f781e98d7a5a39d6abb46b5b7025fa4
sananredworklab <sanan@buddywise.co>
2024-10-13 17:03:01 +0300

    App.js updated.

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+// In App.js in a new project
+//API:
+//https://app.swaggerhub.com/apis/fdcnal/food-data_central_api/1.0.1#/FDC/getFoods
+//Chat gpt secret key: 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
+//Supabase password: fY12@p!_#@ss
+
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,9 +14,7 @@ import Seemore from './sourcecode/Seemore';
 import Information from './sourcecode/Information';
 import About from './sourcecode/About';
 import Icon from 'react-native-vector-icons/Ionicons';
-Icon.loadFont().then(() => {
-  console.log('Ionicons font loaded');
-});
+import Summary from './sourcecode/Summary';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -43,6 +47,32 @@ function RecipieStack() {
   )
 }
 
+function HomeStack() {
+  return(
+    <Stack.Navigator 
+      screenOptions={{
+        headerStyle: {
+          elevation: 0,
+          shadowOpacity: 0,
+          backgroundColor: '#f5f5f5'
+        },
+        headerShadowVisible: false
+      }}
+
+    >
+      <Stack.Screen
+        name='HomeScreen'
+        component={Home}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name='Summary'
+        component={Summary}
+        options={{headerTitle: ''}}
+      />
+    </Stack.Navigator>
+  )}
+
 function App() {
   return (
     <NavigationContainer>
@@ -53,13 +83,15 @@ function App() {
     
           if (route.name === 'Home') {
             iconName = focused ? 'fast-food' : 'fast-food-outline';
-          } else if (route.name === 'RecipieStack') {
+          } else if (route.name === 'Calculator') {
+            iconName = focused ? 'calculator' : 'calculator-outline';
+          } else if (route.name === 'Recipie') {
             iconName = focused ? 'restaurant' : 'restaurant-outline';
           } else if (route.name === 'Information') {
             iconName = focused ? 'information' : 'information-outline';
           } else if (route.name === 'About') {
             iconName = focused ? 'bag-add' : 'bag-add-outline';
-          }
+          }          
     
           return <Icon name={iconName} size={size} color={color} />;
         },
@@ -68,8 +100,9 @@ function App() {
         headerShown: false, // Hides the header for all tabs
       })}
       >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="RecipieStack" component={RecipieStack} />
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Calculator" component={Summary} />
+        <Tab.Screen name="Recipie" component={RecipieStack} />
         <Tab.Screen name="Information" component={Information} />
         <Tab.Screen name="About" component={About} />
       </Tab.Navigator>
@@ -77,4 +110,5 @@ function App() {
   );
 }
 
-export default App;
+export default App
+;
